b47826d6957d5051011a5beabdd61725a8d21ab9
Nayu-mi <129801800+Nayu-mi@users.noreply.github.com>
2026-04-17 03:40:27 +0000

    Altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>É possível chorar nesmo com o sol brilhando lá fora e rir num dia de chuva e ventania.</h1>
+    return <h1>Dizer adeus é o "impossível mais possível", porque você nunca quer dizê-lo, mas seria estupidez não o dizer quando se tem a oportunidade.</h1>
 }
 
 export default Home;
