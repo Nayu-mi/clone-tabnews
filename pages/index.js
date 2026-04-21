15fef63fdfbd45d07525582fc3f6742d150e8931
Nayu-mi <129801800+Nayu-mi@users.noreply.github.com>
2026-04-21 02:29:22 +0000

    adiciona scripts ´lint:check´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Dizer adeus é o "impossível mais possível", porque você nunca quer dizê-lo, mas seria estupidez não o dizer quando se tem a oportunidade.</h1>
+  return (
+    <h1>
+      Dizer adeus é o "impossível mais possível", porque você nunca quer
+      dizê-lo, mas seria estupidez não o dizer quando se tem a oportunidade.
+    </h1>
+  );
 }
 
 export default Home;
